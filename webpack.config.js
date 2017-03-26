c0290af1ad975fccf6dd012dfcc6815ceb942302
rokbar <rokas.barsauskas@ktu.edu>
2017-03-26 22:23:12 +0300

    Webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: [
+    './src/app/index.js'
+  ],
+  output: {
+    path: __dirname + '/public/assets',
+    publicPath: '/assets',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['.', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
