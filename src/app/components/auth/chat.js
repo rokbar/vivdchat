3446779da0083cc68840b6597cdacf81a0cc8e12
rokbar <rokas.barsauskas@ktu.edu>
2017-03-27 14:49:35 +0300

    Send message from React app through Socket.io to Server

--- src/app/components/auth/chat.js
+++ src/app/components/auth/chat.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3000');
 
 class Chat extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: '' };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    console.log(event.target.value);
+    this.setState({ term: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.term);
+    socket.emit('chat message', this.state.term);
+    this.setState({ term: '' });
+  }
+
   componentWillMount() {
     this.props.fetchMessage();
   }
 
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <div>{this.props.message}</div>
+      <div id="messages">
+        <ul></ul>
+        <form onSubmit={this.handleSubmit}>
+          <input id="m" autoComplete="off"
+            value={this.state.term}
+            onChange={this.handleChange} />
+          <button>Send</button>
+        </form>
+      </div>
     );
   }
 }
