df641c324871b160c3d58f190a212c7eb8058a06
rokbar <rokas.barsauskas@ktu.edu>
2017-04-08 00:09:49 +0300

    Boilerplate video recording

--- src/app/components/auth/chat/chat.js
+++ src/app/components/auth/chat/chat.js
@@ -22,6 +22,46 @@ class Chat extends Component {
     });
   }
 
+  componentDidMount() {
+    var startButton = document.getElementById('start');
+    var stopButton = document.getElementById('stop');
+    var liveStream, recorder;
+
+    // Get access to the camera!
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      // Not adding `{ audio: true }` since we only want video now
+      navigator.mediaDevices.getUserMedia({ video: true })
+      .then(function(stream) {
+        liveStream = stream;
+        
+        var liveVideo = document.getElementById('video');
+        liveVideo.src = URL.createObjectURL(stream);
+        liveVideo.play();
+
+        startButton.addEventListener('click', startRecording);
+        stopButton.addEventListener('click', stopRecording);
+      });
+    }
+
+    var startRecording = function() {
+      recorder = new MediaRecorder(liveStream);
+
+      recorder.addEventListener('dataavailable', onRecordingReady);
+
+      recorder.start();
+    }
+
+    var stopRecording = function() {
+      recorder.stop();
+    }
+
+    var onRecordingReady = function(e) {
+      var video = document.getElementById('recorded');
+      video.src = URL.createObjectURL(e.data);
+      video.play();
+    }
+  }
+
   updateChatFromSockets(message) {
     this.props.receiveMessage(message);
   }
@@ -32,9 +72,15 @@ class Chat extends Component {
 
   render() {
     return (
-      <div id="messages">
-        <MessageList messages={this.props.messages} />
-        <MessageInput socket={this.socket} />
+      <div>
+        <video id="video" width="640" height="480" autoPlay></video>
+        <button id="start">Start</button>
+        <button id="stop">Stop</button>
+        <video id="recorded" width="640" height="480" controls loop></video>
+        <div id="messages">
+          <MessageList messages={this.props.messages} />
+          <MessageInput socket={this.socket} />
+        </div>
       </div>
     );
   }
