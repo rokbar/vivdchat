f7607ab52a82479db842fbd9db341f2ae9ff2f5f
rokbar <rokas.barsauskas@ktu.edu>
2017-07-06 17:26:06 +0300

    Removed renderLinks().

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -39,27 +39,6 @@ const Logged = (props) => (
 Logged.muiName = 'IconMenu';
 
 class Header extends Component {
-  renderLinks() {
-    if (this.props.authenticated) {
-      // show a link to sign out
-      return (
-        <li className="nav-item">
-          <Link className="nav-link" to="/signout">Sign Out</Link>
-        </li>
-      );
-    } else {
-      // show a link to sign in or sign up
-      return [
-        <li className="nav-item" key={1}>
-          <Link className="nav-link" to="/signin">Sign In</Link>
-        </li>,
-        <li className="nav-item" key={2}>
-          <Link className="nav-link" to="/signup">Sign Up</Link>
-        </li>
-      ];
-    }
-  }
-
   render() {   
     return (
       <AppBar 
