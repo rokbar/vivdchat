8537345a763f3f25066c229e8d9b003d0ec1353d
rokbar <rokas.barsauskas@ktu.edu>
2017-07-06 11:41:51 +0300

    Added MaterialUI header.

--- src/app/components/app.js
+++ src/app/components/app.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-
 import Header from './header';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import { MuiThemeProvider } from 'material-ui/styles'
+
+injectTapEventPlugin();
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        {this.props.children}
-      </div>
+      <MuiThemeProvider>
+        <div>
+          <Header />
+          {this.props.children}
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
